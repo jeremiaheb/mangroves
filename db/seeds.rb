92a1a52537bdea5a82956b5aff4f05dc02e14047
jeremiaheb <jblondeau@gmail.com>
2013-05-07 14:13:48 -0400

    setup seeding for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
 
-#Animal.delete_all
-#Animal.connection.execute( 'ALTER SEQUENCE animals_id_seq RESTART WITH 1' )
-#open("#{Rails.root}/db/SupportData/SpeciesList.csv") do |animals|
-  #animals.read.each_line do |animal|
-    #SppCode, ScientificName, CommonName = animal.chomp.split(",")
-    #Animal.create( :spp_code => SppCode, :scientific_name => ScientificName, :common_name => CommonName )
-  #end
-#end
+Animal.delete_all
+Animal.connection.execute( 'ALTER SEQUENCE animals_id_seq RESTART WITH 1' )
+open("#{Rails.root}/db/SupportData/SpeciesList.csv") do |animals|
+  animals.read.each_line do |animal|
+    SppCode, ScientificName, CommonName = animal.chomp.split(",")
+    Animal.create( :spp_code => SppCode, :scientific_name => ScientificName, :common_name => CommonName )
+  end
+end
 
 ReferenceSite.connection.execute( 'ALTER SEQUENCE reference_sites_id_seq RESTART WITH 1' )
 open("#{Rails.root}/db/SupportData/refSites.csv") do |animals|
